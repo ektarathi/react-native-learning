7688bfbc550af8d5ded63b301c02abf2b104a5f7
ekta-r <rathiekta2801@gmail.com>
2024-02-23 15:37:38 +0000

    Implemented react navigation

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+//components
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <View style={styles.appContainer}>
-       <CategoriesScreen />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
     </>
   );
@@ -18,5 +27,6 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
+    backgroundColor: "black",
   },
 });
